ffedccef906de7ea47b7288a9f99d92c5745044c
nbc <nicolas@barna.be>
2022-01-11 15:38:50 +0100

    refactor: cleaning files
    * remove _ from file names
    * split files

--- R/get-metadata.R
+++ R/get-metadata.R
@@ -69,26 +69,3 @@ get_metadata <- function(url = NULL) {
   assign("dido_df", dido_df, envir = .dido_env)
   assign("dido_at", dido_at, envir = .dido_env)
 }
-
-#' @noRd
-#' @importFrom readr read_delim locale cols
-
-get_csv <- function(rid,
-                    mil,
-                    query = NULL,
-                    col_types = cols(.default = "c"),
-                    na = c("", "NA")) {
-  path <- paste0("/datafiles/", rid, "/csv")
-  default_query <- list(
-    withColumnName = TRUE,
-    withColumnDescription = FALSE,
-    withColumnUnit = FALSE
-  )
-
-  url <- build_url(path, default_query = default_query, query = query)
-  as_tibble(readr::read_delim(url,
-    delim = ";",
-    locale = locale(decimal_mark = "."),
-    col_types = col_types, na = na
-  ))
-}
